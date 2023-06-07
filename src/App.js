2f5773970f64f006a3910345baebe93d3874fdfd
huy <huy27297@gmail.com>
2023-06-07 23:40:30 +0700

    react h5 player

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 function App() {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
   const [isConverting, setIsConverting] = useState(false);
-  const [audioUrl, setAudioUrl] = useState('');
-  const [error, setError] = useState('');
+  const [audioUrl, setAudioUrl] = useState("");
+  const [error, setError] = useState("");
   const [progress, setProgress] = useState(0);
 
   const convertToMP3 = async () => {
     try {
       setIsConverting(true);
-      setError('');
+      setError("");
       setProgress(0);
-      setAudioUrl('')
+      setAudioUrl("");
       const apiUrl = process.env.REACT_APP_API_URL;
       const response = await axios.get(`${apiUrl}/youTubeToMP3?url=${url}`, {
-        responseType: 'blob',
+        responseType: "blob",
         onDownloadProgress: (progressEvent) => {
-          const percentage = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          const percentage = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
           setProgress(percentage);
         },
       });
 
       setIsConverting(false);
 
-      const audioBlob = new Blob([response.data], { type: 'audio/mpeg' });
+      const audioBlob = new Blob([response.data], { type: "audio/mpeg" });
       const audioUrl = URL.createObjectURL(audioBlob);
 
       setAudioUrl(audioUrl);
     } catch (error) {
       setIsConverting(false);
-      setError('Lỗi khi chuyển đổi URL');
-      setAudioUrl('');
+      setError("Lỗi khi chuyển đổi URL");
+      setAudioUrl("");
     }
   };
 
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>
+    <div style={{ height: "100vh", backgroundColor: "black" }}>
+      <h1 style={{ textAlign: "center", color: "white", marginTop: 0 }}>
         Ứng dụng chuyển đổi YouTube sang MP3
       </h1>
       <div
@@ -67,27 +71,40 @@ function App() {
             transition: "background-color 0.3s ease",
           }}
         >
-          {isConverting ? 'Đang chuyển đổi...' : 'Chuyển đổi'}
+          {isConverting ? "Đang chuyển đổi..." : "Chuyển đổi"}
         </button>
       </div>
       {error && <p style={{ color: "red", textAlign: "center" }}>{error}</p>}
       {audioUrl ? (
-        <div style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "20px",
-        }}>
-          <audio controls>
-            <source src={audioUrl} type="audio/mpeg" />
-            Trình duyệt không hỗ trợ phát định dạng âm thanh này.
-          </audio>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+            alignItems: "center",
+          }}
+        >
+          <AudioPlayer
+            style={{
+              width: "50%",
+            }}
+            volume="0.5"
+            loop
+            autoPlay
+            src={audioUrl}
+            onPlay={(e) => console.log("onPlay")}
+            // other props here
+          />
         </div>
       ) : (
-        <div style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "20px",
-        }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+            color: "white"
+          }}
+        >
           {isConverting && <p>Đang chuyển đổi...</p>}
           {progress > 0 && <p>Tiến trình tải xuống: {progress}%</p>}
         </div>
